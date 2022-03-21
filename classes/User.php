bb4b7681357b5d4265369f49638496855f65177b
Yanellevdb <77410151+Yanellevdb@users.noreply.github.com>
2022-03-21 09:56:04 +0100

    fix: OOP + save data in database

--- classes/User.php
+++ classes/User.php
@@ -1,4 +1,65 @@
 <?php
+
+use LDAP\Result;
+
     class User
     {
+        private $email;
+        private $username;
+        private $password;
+
+        public function setEmail($email){
+            if(empty($email)){
+                throw new Exception("email cannot be empty");
+            }
+            $this->email= $email;
+        }
+
+        public function getEmail(){
+            return $this->email;
+        }
+
+        public function setUsername($username){
+            if(empty($username)){
+                throw new Exception("username cannot be empty");
+            }
+            $this->username= $username;
+        }
+
+        public function getUsername(){
+            return $this->username;
+        }
+
+        public function setPassword($password){
+            if(empty($password)){
+                throw new Exception("password cannot be empty and needs to contain at least 6 characters");
+            }
+            $this->password= $password;
+        }
+
+        public function getPassword(){
+            return $this->password;
+        }
+
+        public function save(){
+            //database connection
+            
+            $conn= new PDO('mysql:host=localhost;dbname=smash', "root", "root");
+
+            //query to save in database
+
+            $statement= $conn->prepare("INSERT INTO users (email, username, password) VALUES (:email, :username, :password)");
+
+            $email= $this->getEmail();
+            $username= $this->getUsername();
+            $password= $this->getPassword();
+
+            $statement->bindValue(":email", $email);
+            $statement->bindValue(":username", $username);
+            $statement->bindValue(":password", $password);
+                
+            $result= $statement->execute();
+
+            return $result;
+        }
     }
--- register.php
+++ register.php
@@ -1,26 +1,45 @@
 <?php 
 
-if(!empty($_POST)){
-    $email= $_POST['email'];
-    $username=$_POST['username'];
-    $options=[
-        'cost' => 12,
-    ];
-    $password= password_hash($_POST['password'], PASSWORD_DEFAULT, $options);
+    include_once("bootstrap.php");
 
-    try{
-        $conn= new PDO('mysql:host=localhost;dbname=smash', "root", "root");
-        $statement= $conn->prepare("INSERT INTO users (email, username, password) VALUES (:email, :username, :password)");
-        $statement->bindValue("email", $email);
-        $statement->bindValue("username", $username);
-        $statement->bindValue("password", $password);
-        
-        $result= $statement->execute();
+    if(!empty($_POST)){
+       /* $email= $_POST['email'];
+        $username=$_POST['username'];
+        $options=[
+            'cost' => 12,
+        ];
+        $password= password_hash($_POST['password'], PASSWORD_DEFAULT, $options);
+
+        try{
+            $conn= new PDO('mysql:host=localhost;dbname=smash', "root", "root");
+            $statement= $conn->prepare("INSERT INTO users (email, username, password) VALUES (:email, :username, :password)");
+            $statement->bindValue("email", $email);
+            $statement->bindValue("username", $username);
+            $statement->bindValue("password", $password);
+            
+            $result= $statement->execute();
+        }
+        catch(Throwable $e){
+            echo $e->getMessage();
+        }*/
+
+        try{
+            $user= new User();
+            $user->setEmail($_POST['email']);
+            $user->setUsername($_POST['username']);
+            $options=[
+                'cost' => 12,
+            ];
+            $password= password_hash($_POST['password'], PASSWORD_DEFAULT, $options);
+            $user->setPassword($password);
+
+            $user->save();
+            $success= "user saved";
+
+        } catch (\Throwable $e){
+            $error= $e->getMessage();
+        }
     }
-    catch(Throwable $e){
-        echo $e->getMessage();
-    }
-}
 
 ?>
 <!DOCTYPE html>
@@ -39,39 +58,27 @@ if(!empty($_POST)){
 
                 <p>*use your Thomas More email address<br>*don't leave any field empty<br>*your password needs to have at least 6 characters</p>
 
+                <?php 
+					if(isset($error)):
+				?>
+
 				<div class="form__error">
 					<p>
-						Sorry. You have to use your Thomas More email address to register. Please try again.
+						Oops, something went wrong! Please check if all fields are filled in and you used a Thomas More email address. 
 					</p>
 				</div>
 
-                <div class="form__error">
-					<p>
-						Sorry. This field cannot be empty. Please try again.
-					</p>
-                </div>
+                <?php endif; ?>
 
 				<div class="form__field">
 					<label for="Email">Email</label>
 					<input type="text" name="email">
 				</div>
 
-                <div class="form__error">
-					<p>
-						Sorry. Your username cannot be empty. Please try again.
-					</p>
-				</div>
-
                 <div class="form__field">
 					<label for="Username">Username</label>
 					<input type="text" name="username">
 				</div>
-
-                <div class="form__error">
-					<p>
-						Sorry. Your password needs to have a minimum of 6 characters. Please try again.
-					</p>
-                </div>
 
 				<div class="form__field">
 					<label for="Password">Password</label>

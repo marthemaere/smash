a02fba1e7cb863a63520430ba2a1b55d52c0d5fe
Ellen <ellen.deveth@hotmail.com>
2022-03-28 12:02:47 +0200

    feat: add error message if account doesn't exist

--- forgotPassword.php
+++ forgotPassword.php
@@ -1,6 +1,14 @@
 <?php
-    include_once(__DIR__ . "bootstrap.php");
+    include_once(__DIR__ . "/bootstrap.php");
     
+    if (!empty($_POST)) {
+        $email = $_POST['email'];
+        try {
+            $user = User::hasAccount($email);
+        } catch (Throwable $e) {
+            $error = $e->getMessage();
+        }
+    }
    
 ?><!DOCTYPE html>
 <html lang="en">
@@ -15,10 +23,15 @@
       <h1>Reset your password</h1>
       <p>An email will be send to reset your password</p>
 
+        <?php if (isset($error)):?>
+            <div>
+                <p><?php echo $error; ?></p>
+            </div>
+        <?php endif;?>
     
     <div class="form form--login">
-      <label for="username">Email</label>
-      <input type="text" id="username" name="username">
+      <label for="email">Email</label>
+      <input type="text" id="email" name="email">
       <input type="submit" class="btn" id="btnSubmit" value="Send email" name="forgot_password">
     </div>
 </body>

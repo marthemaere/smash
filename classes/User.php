66f145c837e325096bb35c094e9a4cd39035fa5d
Ellen <ellen.deveth@hotmail.com>
2022-03-28 12:25:48 +0200

    feat: add link to email

--- classes/User.php
+++ classes/User.php
@@ -158,37 +158,41 @@ class User
 
     public static function sendPasswordResetEmail($emailTo)
     {
-           
-            //send email from email to user
+        //get random code
+        $code = uniqid(true);
+        //get connection
+        $conn = Db::getInstance();
+        //send email from email to user
         $mail = new PHPMailer(true);
 
         try {
             //Server settings
-                // $mail->SMTPDebug = 2;
-                $mail->isSMTP();                                            //Send using SMTP
-                $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                $mail->Username   = 'smash.weareimd@gmail.com';                     //SMTP username
-                $mail->Password   = '4p7h*LN2j4WYj^';                               //SMTP password
-                $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-                $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            // $mail->SMTPDebug = 2;
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'smash.weareimd@gmail.com';                     //SMTP username
+            $mail->Password   = '4p7h*LN2j4WYj^';                               //SMTP password
+            $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
                 
-                //Recipients
+            //Recipients
             $mail->setFrom('smash.weareimd@gmail.com', 'Smash');
             $mail->addAddress($emailTo);     //Add a recipient
             $mail->addReplyTo('no-reply@gmail.com', 'No reply');
                 
 
             //Content
-                $mail->isHTML(true);                                  //Set email format to HTML
-                $mail->Subject = "Password reset link ";
+            //get url where user is on + add token
+            $url = "http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . "/resetPassword.php?code=$code";
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = "Password reset link ";
             $mail->Body    = "<h1> You requested to reset your password </h1>
-                                    click <a href=# > this link </a> to do so";
+                                    click <a href='$url' > this link </a> to do so";
             $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
             $mail->send();
-            echo "message sent";
         } catch (Exception $e) {
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         };

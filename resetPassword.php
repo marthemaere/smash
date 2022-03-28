f595ca5e25bab0719af23f334ae734d4878f2433
Ellen <ellen.deveth@hotmail.com>
2022-03-28 12:40:02 +0200

    feat: add form to reset password

---
+++ resetPassword.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+      <h1>New Password</h1>
+
+    
+    <div class="form form--login">
+      <label for="password">New password</label>
+      <input type="text" id="password" name="password">
+      <input type="submit" class="btn" id="btnSubmit" value="Save password">
+    </div>
+</body>
+</html>

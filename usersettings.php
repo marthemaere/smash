ac694e543d750e19a9b21bec40f95bbb9683d1d2
Fien Gérardi <fien.gerardi@gmail.com>
2022-03-29 16:22:04 +0200

    style: add class to delete account

--- usersettings.php
+++ usersettings.php
@@ -25,14 +25,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/custom.scss">
+    <link rel="stylesheet" href="styles/custom.scss">
     <title>Settings</title>
 </head>
 <body>
     <?php include_once('header.php'); ?>
     <div class="container">
         <div class="header">
-            <h1>Settings</h1>
+            <h1 class="text-primary">Settings</h1>
             <img src="profile_pictures/<?php echo $userDataFromId['profile_pic']; ?>" class="img-thumbnail rounded-circle" alt="profile picture">
         </div>
 
@@ -43,7 +43,7 @@
                     <a class="list-group-item list-group-item-action" data-bs-toggle="list" href="#profile" role="tab" aria-controls="profile">Edit profile</a>
                     <a class="list-group-item list-group-item-action" data-bs-toggle="list" href="#password" role="tab" aria-controls="password">Change password</a>
                     <a class="list-group-item list-group-item-action" data-bs-toggle="list" href="#socials" role="tab" aria-controls="socials">Social profiles</a>
-                    <a class="list-group-item list-group-item-action" href="#">Remove account</a>
+                    <a class="list-group-item list-group-item-action text-danger" href="#">Remove account</a>
                 </div>
             </div>
 
@@ -53,17 +53,17 @@
                         <form action="">
                             <fieldset>
                                 <label for="username" class="form-label">Username</label>
-                                <input type="text" class="form-control" name="name" id="name" readonly>
+                                <input type="text" class="form-control" name="name" value="" id="name" readonly>
                             </fieldset>
 
                             <fieldset>
                                 <label for="name" class="form-label">Name</label>
-                                <input type="text" class="form-control" name="name" id="name" readonly>
+                                <input type="text" class="form-control" name="name" value="" id="name" readonly>
                             </fieldset>
 
                             <fieldset>
                                 <label for="email" class="form-label">Email</label>
-                                <input type="text" class="form-control" name="email" id="email" readonly>
+                                <input type="text" class="form-control" name="email" value="" id="email" readonly>
                             </fieldset>
                         </form>
                     </div>

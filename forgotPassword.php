aec701e505e70b9d31d889f9e92e6e79858a7471
Fien Gérardi <fien.gerardi@gmail.com>
2022-04-04 11:43:44 +0200

    style (user registration): add css styling to login, signup and reset password pages

--- forgotPassword.php
+++ forgotPassword.php
@@ -20,23 +20,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/custom.css">
+    <title>Forgot password</title>
 </head>
 <body>
-    <form action="" method="post">
-      <h1>Reset your password</h1>
-      <p>An email will be send to reset your password</p>
+    <div class="resetPassword row">
+        <div class="reset--image col">
+            <a class="navbar-brand">Smasssh</a>
+        </div>
+        <div class="reset--form col">
+            <div class="form form--reset">
+                <form action="" method="post">
+                    <h1>Reset your password</h1>
+                    <p class="alert alert-info">An email will be send to reset your password</p>
 
-        <?php if (isset($error)):?>
-            <div>
-                <p><?php echo $error; ?></p>
+                    <?php if (isset($error)):?>
+                    <div class="alert alert-danger">
+                        <p><?php echo $error; ?></p>
+                    </div>
+                    <?php endif;?>
+
+                    <div class="form-floating my-3">
+                        <input type="text" class="form-control" id="email" placeholder="Email"  name="email">
+                        <label for="email">Email</label>
+                    </div>
+                    <div class="d-grid">
+                        <input type="submit" class="btn btn-dark" id="btnSubmit" value="Send email" name="forgot_password">
+                    </div>
+                </form>
+                <div class="text-center my-2">
+                    <a href="login.php" class="link-dark">Go back to login page</a>
+                </div>
             </div>
-        <?php endif;?>
-    
-    <div class="form form--login">
-      <label for="email">Email</label>
-      <input type="text" id="email" name="email">
-      <input type="submit" class="btn" id="btnSubmit" value="Send email" name="forgot_password">
+        </div>
     </div>
 </body>
 </html>
--- register.php
+++ register.php
@@ -25,49 +25,54 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles/custom.css">
   <title>Smasssh</title>
-  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<div class="smashRegister">
-		<div class="form form--login">
-			<form action="" method="post">
-                <h1>Hi! Join our Smash-community!</h1>
-				<h2 form__title>Sign Up</h2>
+	<div class="register row">
+        <div class="register--image col">
+            <a class="navbar-brand">Smasssh</a>
+        </div>
 
-                <p>*use your Thomas More email address<br>*don't leave any field empty<br>*your password needs to have at least 6 characters</p>
+        <div class="register--form col">
+            <div class="form form--register">
+                <form action="" method="post">
+                    <h1>Hi there! Join the<br>community now!</h1>
+                    <!-- <p>*use your Thomas More email address<br>*don't leave any field empty<br>*your password needs to have at least 6 characters</p> -->
 
-                <?php
-                    if (isset($error)):
-                ?>
+                    <?php if (isset($error)): ?>
+                    <div class="alert alert-danger">
+                        <p>
+                            Oops, something went wrong! Please check if all fields are filled in and you used a Thomas More email address. 
+                        </p>
+                    </div>
+                    <?php endif; ?>
 
-				<div class="form__error">
-					<p>
-						Oops, something went wrong! Please check if all fields are filled in and you used a Thomas More email address. 
-					</p>
-				</div>
+                    <div class="form-floating my-3">
+                        <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" value="r012345@student.thomasmore.be" name="email">
+                        <label for="floatingInput">Email</label>
+                    </div>
 
-                <?php endif; ?>
+                    <div class="form-floating my-3">
+                        <input type="text" class="form-control" id="floatingInput" placeholder="Username" name="username">
+                        <label for="floatingInput">Username</label>
+                    </div>
 
-				<div class="form__field">
-					<label for="Email">Email</label>
-					<input type="text" name="email">
-				</div>
-
-                <div class="form__field">
-					<label for="Username">Username</label>
-					<input type="text" name="username">
-				</div>
-
-				<div class="form__field">
-					<label for="Password">Password</label>
-					<input type="password" name="password">
-				</div>
-				<div class="form__field">
-					<input type="submit" value="Sign up" class="btn btn--primary">	
-				</div>
-			</form>
-		</div>
+                    <div class="form-floating my-3">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
+                        <label for="floatingPassword">Password</label>
+                    </div>
+                    <div class="d-grid">
+                        <input type="submit" value="Sign up" class="btn btn-dark">	
+                    </div>
+                </form>
+                <p class="text-center my-2">Already have an account? <a href="login.php" class="link-primary">Log in</a></p>
+            </div>
+        </div>
 	</div>
 </body>
 </html>

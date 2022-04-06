14f07d616ade858805878204d90d45d6bc6a1160
Jade Apers <jadeapers@telenet.be>
2022-04-06 20:56:37 +0200

    feat: add class post with title, image and tags

--- classes/Post.php
+++ classes/Post.php
@@ -1,8 +1,93 @@
 <?php
     class Post
     {
+        private $title;
+        private $image;
+        private $freetags;
+        private $upload;
+
         public static function getAll(){
         $conn = Db::getInstance();
         $result = $conn->query("select * from posts");
         return $result->fetchAll();
+
+        /**
+         * Get the value of title
+         */ 
+        public function getTitle()
+        {
+                return $this->title;
+        }
+
+        /**
+         * Set the value of title
+         *
+         * @return  self
+         */ 
+        public function setTitle($title)
+        {
+                $this->title = $title;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of image
+         */ 
+        public function getImage()
+        {
+                return $this->image;
+        }
+
+        /**
+         * Set the value of image
+         *
+         * @return  self
+         */ 
+        public function setImage($image)
+        {
+                $this->image = $image;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of freetags
+         */ 
+        public function getFreetags()
+        {
+                return $this->freetags;
+        }
+
+        /**
+         * Set the value of freetags
+         *
+         * @return  self
+         */ 
+        public function setFreetags($freetags)
+        {
+                $this->freetags = $freetags;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of upload
+         */ 
+        public function getUpload()
+        {
+                return $this->upload;
+        }
+
+        /**
+         * Set the value of upload
+         *
+         * @return  self
+         */ 
+        public function setUpload($upload)
+        {
+                $this->upload = $upload;
+
+                return $this;
+        }
     }

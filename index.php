bf94b1cb32deb71e2b72b1c7c4cda81b03aeda1f
Jade Apers <jadeapers@telenet.be>
2022-04-17 15:42:58 +0200

    feat: added bootstrap to index page

--- index.php
+++ index.php
@@ -21,7 +21,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/custom.css">
     <title>Feed</title>
-    <a href="/php/smash/uploadProject.php">Upload Project</a>
 
 
 
@@ -34,6 +33,8 @@
         <img class="empty-state-picture" src="assets\images\empty-box.svg" alt="emptystate">
         <p> No projects were found. </p> </div>'
     ?>
+    <div class="d-flex justify-content-center"><a class="btn btn-primary" href="/php/smash/uploadProject.php">Upload Project</a></div>
+    
     <?php require_once("footer.php"); ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>

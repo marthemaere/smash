d1f3bb2cc49da4a1379263a726f6777037f5fa2d
Fien Gérardi <fien.gerardi@gmail.com>
2022-05-01 21:51:10 +0200

    refactor: change layour of user settings

--- changePassword.php
+++ changePassword.php
@@ -33,22 +33,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/custom.css">
+    <?php include_once('style.php'); ?>
     <title>Settings</title>
     
 </head>
 <body>
     <?php include_once('header.php'); ?>
     <div class="container">
-        <div class="header">
-            <h1 class="">Settings</h1>
-            <img src="profile_pictures/<?php echo $userDataFromId['profile_pic']; ?>" class="img-thumbnail rounded-circle" alt="profile picture">
-        </div>
-        
+        <p class="ps-3 pt-5 text-muted"><?php echo $userDataFromId['username']; ?> / Settings / Password</p>
+        <h1 class="ps-3">Change password</h1>
+
         <div class="row">
             <div class="col-3">
             <div class="list-group" id="list-tab" role="tablist">
@@ -63,7 +57,6 @@
              <!-- ChangePassword -->
              <div class="col-9">
              <div class="tab-pane fade active show" id="password" role="tabpanel" aria-labelledby="password-list">
-                        <h2 class="mb-4">Change password</h2>
                         <?php if (isset($error)): ?>
                             <p class="alert alert-danger"><?php echo $error; ?></p>
                         <?php endif; ?>
--- socialAccounts.php
+++ socialAccounts.php
@@ -35,21 +35,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/custom.css">
+    <?php include_once('style.php'); ?>
     <title>Settings</title>
     
 </head>
 <body>
     <?php include_once('header.php'); ?>
     <div class="container">
-        <div class="header">
-            <h1 class="">Settings</h1>
-            <img src="profile_pictures/<?php echo $userDataFromId['profile_pic']; ?>" class="img-thumbnail rounded-circle" alt="profile picture">
-        </div>
+        <p class="ps-3 pt-5 text-muted"><?php echo $userDataFromId['username']; ?> / Settings / Socials</p>
+        <h1 class="ps-3">Share your socials</h1>
 
         <div class="row">
             <div class="col-3">
@@ -64,10 +58,6 @@
 
             <div class="col-9">
                 <div class="" id="socials" role="tabpanel" aria-labelledby="socials-list">
-                    <h2 class="mb-4">Share social links</h2>
-                    <p>
-                        Add links of your other social media accounts to your profile to share your work.
-                    </p>
                     <?php if (isset($error)): ?>
                         <p class="alert alert-danger"><?php echo $error; ?></p>
                     <?php endif; ?>
@@ -75,7 +65,7 @@
                         <p class="alert alert-success"><?php echo $success; ?></p>
                     <?php endif; ?>
                     <form action="" method="post">
-                            <div class="social-link-item my-3">
+                            <div class="social-link-item">
                                 <label for="linkedin" class="form-label">LinkedIn</label>
                                 <input type="url" class="form-control" name="linkedin" id="linkedin" value="<?php echo $userDataFromId['social_linkedin']; ?>">
                             </div>

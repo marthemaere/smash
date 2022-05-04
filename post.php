33b8e6b70d76f60596894448b805de4e2c3cfee3
Fien Gérardi <fien.gerardi@gmail.com>
2022-05-04 22:11:17 +0200

    feat: overwrite own code with last code of post.php

--- post.php
+++ post.php
@@ -24,8 +24,10 @@
         }
     }
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -33,91 +35,105 @@
     <?php include_once('style.php'); ?>
     <title>Post</title>
 </head>
+
 <body>
     <?php include_once('header.php'); ?>
-    <div class="container">
-            <div class="row">
-                <?php if (isset($success)): ?>
-                    <div class="alert alert-success m-2" role="alert">
-                        <?php echo $success; ?>
-                    </div>
-                <?php endif; ?>
-                <?php if (isset($error)): ?>
-                    <div class="alert alert-danger" role="alert">
-                        <?php echo $error; ?>
-                    </div>
-                <?php endif; ?>
-                <!-- are you sure alert -->
-                <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalToggleLabel">Are you sure you want to report this post?</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <form action="" method="post">
-                                <div class="modal-footer">
-                                    <button class="btn btn-outline-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">No</button>
-                                    <input id="report-post" data-postId=`$key` type="submit" value="yes" name="report" class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <!-- are you sure alert -->
-                
-                <div class="d-flex align-items-center p-3">
-                    <img src="profile_pictures/<?php echo htmlspecialchars($projectData['profile_pic']); ?>" class="img-profile-post">
-                    <a href="profile.php?p=<?php echo htmlspecialchars($projectData['user_id']);?>">
-                        <h4 class="pt-2 ps-2"><?php echo htmlspecialchars($projectData['username']);?></h4>
-                    </a>
-                </div>   
-                
-                <div class="col-9">
-                        <h2><?php echo htmlspecialchars($projectData['title']); ?></h2>
-                        <div class="d-flex align-items-center justify-content-between">
-                            <p class="pe-4"><?php echo htmlspecialchars($projectData['description']); ?> <span class="link-primary"><?php echo htmlspecialchars($projectData['tags']); ?></span></p>
-                            <div class="d-flex justify-content-between align-items-center">   
-                                <form  class="d-flex align-items-center" action="" method="post">
-                                    <div class="btn btn-primary d-flex">
-                                        <img src="assets/images/empty-heart.svg" class="btn-icon-like">
-                                        <input type="submit" value="Like" class="btn p-0" name="like">
-                                        <p class="num-of-likes p-2"> 1</p>
-                                    </div>
-                                </form>
-                                
-                                <a class="btn btn-outline-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Report</a> 
-                            </div>
-                        </div>
-                </div>
+    <div class="container my-5">
+        <div class="row">
+            <?php if (isset($success)): ?>
+            <div class="alert alert-success m-2" role="alert">
+                <?php echo $success; ?>
             </div>
+            <?php endif; ?>
+            <?php if (isset($error)): ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo $error; ?>
+            </div>
+            <?php endif; ?>
 
-            <div class="row">
-                <div class="col-9">
-                    <img src="uploaded_projects/<?php echo htmlspecialchars($projectData['image']);?>" width="100%" height="75%" class="img-project-post" style="object-fit:cover" >
-                </div>     
-
-                <div class="col-3 col-lg-3">
-                    <h3>Comments</h3>
-                    <?php if (isset($emptystate)): ?>
-                        <p class="empty-state">No comments yet</p>
-                    <?php else: ?>
-                    <ul class="">
-                        <?php foreach ($comments as $c): ?>
-                            <li class=""><?php echo htmlspecialchars($c['text']); ?></li>
-                        <?php endforeach; ?>
-                    </ul>
-                    <?php endif; ?>
-                    <div class="row d-flex justify-content-between">
-                        <form class="form" action="" method="post">
-                            <input class="form-control col" type="text" placeholder="make a comment">
-                            <input type="submit" value="Send" class="btn btn-primary col col-lg-3" name="comment">
+            <!-- are you sure alert -->
+            <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"
+                tabindex="-1">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalToggleLabel">Are you sure you want to report this
+                                post?</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <form action="" method="post">
+                            <div class="modal-footer">
+                                <button class="btn btn-outline-primary" data-bs-target="#exampleModalToggle2"
+                                    data-bs-toggle="modal">No</button>
+                                <input id="report-post" data-postId=`$key` type="submit" value="yes" name="report"
+                                    class="btn btn-primary" data-bs-target="#exampleModalToggle2"
+                                    data-bs-toggle="modal">
+                            </div>
                         </form>
                     </div>
                 </div>
             </div>
-            
-        
+            <!-- are you sure alert -->
+
+            <div class="d-flex align-items-center py-2">
+                <img src="profile_pictures/<?php echo $projectData['profile_pic']; ?>" class="img-profile-post">
+                <a href="profile.php?p=<?php echo $projectData['user_id'];?>">
+                    <h4 class="pt-2 ps-2"><?php echo $projectData['username'];?></h4>
+                </a>
+            </div>
+
+            <div class="col-8 py-0">
+                <div class="d-flex align-items-start justify-content-between">
+                    <div>
+                        <h2><?php echo $projectData['title']; ?></h2>
+                        <p class="pe-4"><?php echo $projectData['description']; ?> <span
+                                class="link-primary"><?php echo $projectData['tags']; ?></span></p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <form class="d-flex align-items-center" action="" method="post">
+                            <div class="btn btn-primary d-flex align-items-center">
+                                <img src="assets/images/empty-heart.svg" class="btn-icon-like">
+                                <input type="submit" value="Like" class="btn p-0 ps-1" name="like">
+                                <p class="num-of-likes"> 1</p>
+                            </div>
+                        </form>
+                        <a class="btn btn-outline-primary ms-2" data-bs-toggle="modal" href="#exampleModalToggle"
+                            role="button">Report</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-8">
+                <img src="uploaded_projects/<?php echo $projectData['image'];?>" width="100%" height="100%"
+                    class="img-project-post" style="object-fit:cover">
+            </div>
+
+            <div class="col-4 col-lg-4 d-flex align-content-between flex-wrap">
+                <div>
+                    <h3>Comments</h3>
+                    <?php if (isset($emptystate)): ?>
+                    <p class="">There are no comments for this project.</p>
+                    <?php else: ?>
+                    <ul class="">
+                        <?php foreach ($comments as $c): ?>
+                        <li class=""><?php echo $c['text']; ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <?php endif; ?>
+                </div>
+
+                <form class="" action="" method="post">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Place a comment"
+                            aria-label="Place a comment" aria-describedby="button-addon2">
+                        <input class="btn btn-outline-primary btn-icon-search" type="submit" name="submit-search"
+                            id="button-addon2" value=">">
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
     <?php require_once("footer.php"); ?>

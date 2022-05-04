85f9166187f3e227872e2f50f846d68e95c100f0
Fien Gérardi <fien.gerardi@gmail.com>
2022-05-04 11:40:22 +0200

    style: add css layout to upload project page

--- uploadProject.php
+++ uploadProject.php
@@ -24,8 +24,6 @@ if (!empty($_POST)) {
         $tags->addTagsToDatabase($id);
         
         header("Location: index.php");
-
-
     } catch (\Throwable $e) {
         $error = $e->getMessage();
     }
@@ -39,53 +37,52 @@ if (!empty($_POST)) {
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/custom.css">
+    <?php include_once('style.php'); ?>
     <title>Upload your project</title>
 </head>
 <body>
     <?php require_once("header.php"); ?>
-    <div class="upload-intro"> 
-        <h1> What are you working on? Show us all of it! #excited 🤩 </h1> 
-        <p> Upload your project. </p>
+
+    <div class="container py-4">
+        <a href="index.php" class="btn btn-outline-primary">Cancel</a>
+
+        <div class="upload-intro text-center pt-4">
+            <h1>What are you working on?</h1>
+            <p>Show us what you got! Upload your mindblowing project.</p>
+        </div>
+
+        <div class="col-7 py-5 m-auto">
+            <?php if (isset($error)):?>
+            <div class="alert alert-danger"><?php echo $error; ?></div>
+            <?php endif;?>
+
+            <form class="uploadzone" action="#" method="POST" enctype="multipart/form-data">
+                <fieldset>
+                    <!-- <label for="floatingInput">Give your project a name</label> -->
+                    <input type="text" class="form-control" id="floatingInput" name="title" placeholder="Give your project a name">
+                </fieldset>
+
+                <fieldset>
+                    <!-- <label for="title">Tell us more about your project</label> -->
+                    <textarea name="description" class="form-control" id="description" cols="50" rows="3" placeholder="Tell us more about your project (but not a chapter)"></textarea>
+                </fieldset>
+
+                <fieldset>
+                    <!-- <label for="tags">Add tags to your project</label> -->
+                    <input type="text" class="form-control" id="tags" name="tags" placeholder="Give it some tags like #branding">
+                    <div class="form-text">Don't forget the famous '#' before your tag</div>
+                </fieldset>
+
+                <fieldset>
+                    <label class="form-label" for="image"></label>
+                    <input type="file" class="form-control" id="customFile" name="file">
+                    <div class="form-text">JPG or PNG. Max size of 2MB</div>
+                </fieldset>
+
+                <input class="btn btn-primary col-12" type="submit" value="Upload masterpiece" name="submit">
+            </form>
+        </div>
     </div>
-    <?php if (isset($error)):?>
-        <div class="alert alert-danger"><?php echo $error; ?></div>
-    <?php endif;?>
-  
-
-    <div class="login--form col">
-    <div class="form form--login">
-    <form class="uploadzone" action="#" method ="POST" enctype="multipart/form-data">
-
-        <fieldset>  
-        <label for="floatingInput">Give your project a name</label>
-        <input type="text" class="form-control" id="floatingInput" name="title" > <br>
-        </fieldset>
-
-        <fieldset>
-        <label for="title">Tell us more about your project</label>
-        <textarea name="description" class="form-control" id="description" cols="50" rows="3" ></textarea>
-        </fieldset>
-
-
-        <fieldset>
-        <label for="tags">Add tags to your project</label>
-        <input type="text" class="form-control" id="tags" name="tags" > <br>
-        </fieldset>
-
-        <fieldset>
-        <label class="form-label" for="image"></label>
-        <input type="file" class="form-control" id="customFile" name="file"> <br>
-        <div class="form-text">JPG or PNG. Max size of 2MB</div>
-        </fieldset>
-
-        <input class="btn btn-dark" type="submit" value="Upload project" name="submit">
-        </div>
-        </div>
 
     <?php require_once("footer.php"); ?>
 </body>

40f5a47923361a6a3f14056cf2c827d93cc79c9a
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-07 21:58:12 +0200

    feat: add javascript to fetch ajax when post reported

---
+++ javascript/report.js
@@ -0,0 +1,25 @@
+document.querySelector("#report-post").addEventListener("click", function(e) {
+    e.preventDefault();
+    let postid = e.target.dataset.postid;
+
+    
+    let formData = new FormData();
+    formData.append("postid", postid);
+
+    fetch("ajax/report.php",  {
+        method: "POST",
+        body: formData
+    })
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            document.querySelector("#report-success").innerHTML = result.message;
+            document.querySelector("#report-success").classList = "alert alert-success m-2 visible";
+            document.querySelector('#report-btn').innerHTML = "Reported";
+            document.querySelector('#report-btn').classList = "btn btn-danger disabled";
+            
+        })
+        .catch(error => {
+            console.log(error);
+        })
+});

49e243fceeae7c55e48ca4da0fdd1c6ba28b140b
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-10 17:40:32 +0200

    refactor: clean alert messages to reset password

--- classes/Mailer.php
+++ classes/Mailer.php
@@ -36,14 +36,14 @@
         public function hasAccount()
         {
             $conn = Db::getInstance();
-            $statement = $conn->prepare("select * from users where email = :email");
+            $statement = $conn->prepare("select * from users where email OR second_email = :email");
             $statement->bindValue(":email", $this->email);
             $statement->execute();
             $user = $statement->fetch(PDO::FETCH_ASSOC);
             if ($user) {
                 return true;
             } else {
-                throw new Exception("user doesn't exist");
+                throw new Exception("This user doesn't exist.");
             }
         }
 

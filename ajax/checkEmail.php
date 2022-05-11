e3304ff85ae4bcc95ab1ddc8ed3b30e2fcac441d
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-11 22:17:11 +0200

    fix: remove database errors from ajax file

--- ajax/checkEmail.php
+++ ajax/checkEmail.php
@@ -1,17 +1,17 @@
 <?php
 include_once(__DIR__.'/../bootstrap.php');
-include_once(__DIR__ . "/Db.php");
 
-if(isset($_POST['type']) == 1){
+
+if (isset($_POST['type']) == 1) {
     $email = $_POST['email'];
     $conn = DB::getInstance();
     $statement = $conn->prepare("select * from users where email = :email");
     $statement->bindValue("email", $email);
     $statement->execute();
     $rowcount = $statement->rowCount();
-    if($rowcount > 0){
+    if ($rowcount > 0) {
         echo "<span style='color: red;' class='status-not-available'> Email Not Available.</span>";
-    }else{
-         echo "<span style='color: green' class='status-available'> Email Available.</span>";
+    } else {
+        echo "<span style='color: green' class='status-available'> Email Available.</span>";
     }
 }

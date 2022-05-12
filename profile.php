070d0d88802e24fcff7b10d89652f4d9c53bae41
Jade Apers <jadeapers@telenet.be>
2022-05-12 08:08:18 +0200

    fix: trying to console log the dataset postId

--- profile.php
+++ profile.php
@@ -137,7 +137,7 @@
                             </div>
                             <div class="d-flex justify-content-between align-items-center">
                                 <a href="" class="link-dark">View comments</a>
-                                <a href="" id="smashed" name="smashed" class="btn btn-outline-primary" data-postId="3"> Smash </a>
+                                <a href="#" id="smashed" name="smashed" class="btn btn-outline-primary" data-postId="3"> Smash </a>
                             </div>
                         </div>
                     <?php endforeach; ?>
@@ -151,6 +151,7 @@
     <script src="javascript/follow.js"></script>
     <script src="javascript/report-user.js"></script>
     <script src="javascript/smashed.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>

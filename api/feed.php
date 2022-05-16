b5a8ce1fd73a1532c5f88981efe42358dede0dfe
Fien Gérardi <78747974+fgrardi@users.noreply.github.com>
2022-05-16 20:44:21 +0200

    feat: show json result in feed.php with link to specific post

--- api/feed.php
+++ api/feed.php
@@ -3,10 +3,13 @@
     
     $api = new Api();
     $posts = $api->get40LatestPosts();
-    $postLink = "http://localhost/api/feed.php?p=";
-    //array_push($posts, $postLink);
-    //var_dump($posts);
-    $response = ["status" => "success", "data" => ["posts" => $posts]];
+    
+    $response = [
+        "status" => "success",
+        "data" => [
+            "posts" => $posts
+        ]
+    ];
 
     header("Content-Type: application/json");
     echo json_encode($response);
--- classes/Api.php
+++ classes/Api.php
@@ -4,7 +4,7 @@
         public function get40LatestPosts()
         {
             $conn = Db::getInstance();
-            $statement = $conn->query('SELECT id, title, image, date, description, isShowcase FROM posts ORDER BY id DESC LIMIT 40');
+            $statement = $conn->query('SELECT id, title postTitle, description postDescription ,CONCAT(\'http://localhost/api/feed.php?p=\', id) as postLink, date postDateAdded, image postImageThumbnail FROM posts ORDER BY id DESC LIMIT 40');
             $statement->execute();
             $result = $statement->fetchAll(PDO::FETCH_ASSOC);
             return $result;

565d9d86a5270a4fb36e87d4786a9a27eb3e824a
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-17 16:34:15 +0200

    refactor: change functions to loop through tags

--- classes/Post.php
+++ classes/Post.php
@@ -169,7 +169,7 @@ class Post
     public static function getPostDataFromId($id)
     {
         $conn = Db::getInstance();
-        $statement = $conn->prepare("SELECT * FROM posts INNER JOIN users on posts.user_id = users.id INNER JOIN tags on tags.post_id = posts.id WHERE posts.id = :id");
+        $statement = $conn->prepare("SELECT * FROM posts INNER JOIN users on posts.user_id = users.id WHERE posts.id = :id");
         $statement->bindValue(':id', $id);
         $statement->execute();
         $result = $statement->fetch(PDO::FETCH_ASSOC);
@@ -179,7 +179,17 @@ class Post
     public static function getPosts($sorting, $start, $limit)
     {
         $conn = Db::getInstance();
-        $statement = $conn->prepare("select * from posts INNER JOIN users ON posts.user_id = users.id LEFT JOIN tags on tags.id= posts.id ORDER BY `date` $sorting LIMIT $start, $limit");
+        $statement = $conn->prepare("SELECT * from posts INNER JOIN users ON posts.user_id = users.id ORDER BY `date` $sorting LIMIT $start, $limit");
+        $statement->execute();
+        $result = $statement->fetchAll();
+        return $result;
+    }
+
+    public static function getTagsFromPost($postId)
+    {
+        $conn = Db::getInstance();
+        $statement = $conn->prepare("SELECT * FROM tags WHERE post_id = :postId");
+        $statement->bindValue(':postId', $postId);
         $statement->execute();
         $result = $statement->fetchAll();
         return $result;
@@ -236,7 +246,7 @@ class Post
     {
         $conn = Db::getInstance();
         $statement = $conn->prepare(
-            "SELECT * FROM posts INNER JOIN users ON posts.user_id = users.id INNER JOIN tags ON tags.post_id = posts.id WHERE posts.isShowcase=1 AND posts.user_id = :id"
+            "SELECT * FROM posts INNER JOIN users ON posts.user_id = users.id WHERE posts.isShowcase=1 AND posts.user_id = :id"
         );
         $statement->bindValue(':id', $id);
         $statement->execute();

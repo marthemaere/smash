d09dd47d6835d5372c892232dc650b0d56f61f92
Fien Gérardi <78747974+fgrardi@users.noreply.github.com>
2022-05-17 17:14:53 +0200

    feat: make footer responsive for smaller screens

--- footer.php
+++ footer.php
@@ -1,18 +1,18 @@
 <div class="bg-light">
     <footer class="container pt-4">
         <div class="row">
-            <div class="col-5">
+            <div class="col-sm-12 col-md-6">
                 <a href="#" class="navbar-brand my-2">Smasssh</a>
                 <p class="py-3 pe-5">Smasssh is the educative community for students of Interactive Multimedia Design of Thomas More to share and like projects.</p>
             </div>
-            <div class="col">
+            <div class="col-sm-12 col-md-2">
                 <h4>Pages</h4>
                 <ul class="nav flex-column">
                     <li class="nav-item"><a href="index.php" class="nav-link px-0 text-muted">Home</a></li>
                     <li class="nav-item"><a href="profile.php?p=<?php echo $userDataFromId['id']; ?>" class="nav-link px-0 text-muted">Profile</a></li>
                 </ul>
             </div>
-            <div class="col">
+            <div class="col-sm-12 col-md-2">
                 <h4>Profile</h4>
                 <ul class="nav flex-column">
                     <li class="nav-item"><a href="account-info.php" class="nav-link px-0 text-muted">Account info</a></li>    
@@ -20,7 +20,7 @@
                     <li class="nav-item"><a href="change-password.php" class="nav-link px-0 text-muted">Change password</a></li>
                 </ul>
             </div>
-            <div class="col">
+            <div class="col-sm-12 col-md-2">
                 <h4>Account</h4>
                 <ul class="nav flex-column">
                     <li class="nav-item"><a href="logout.php" class="nav-link px-0 text-muted">Log out</a></li>

14aa263f64ae5d575e60ac7a838650709cf07837
Fien Gérardi <78747974+fgrardi@users.noreply.github.com>
2022-05-18 14:34:27 +0200

    fix: add closing tag of div for fixing html code

--- smashedProjects.php
+++ smashedProjects.php
@@ -21,8 +21,7 @@ if (!isset($_SESSION['id'])) {
 if (empty($posts)) {
     $emptyState = true;
 }
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -37,20 +36,20 @@ if (empty($posts)) {
 <body>
     <?php include_once('header.php'); ?>
     <div class="container">
-        <div class="row p-3">
-            <div id="report-success" class="invisible" role="alert">
-            </div>
-
-            <?php if (isset($error)) : ?>
+        <div id="report-success" class="invisible" role="alert"></div>
+        <?php if (isset($error)) : ?>
+            <div class="row p-3">
                 <div class="alert alert-danger" role="alert">
                     <?php echo $error; ?>
                 </div>
-            <?php endif; ?>
-        </div>
+            </div>
+        <?php endif; ?>
         <div class="row d-flex align-items-center">
             <div class="col">
                 <img src="profile_pictures/<?php echo $userData['profile_pic']; ?>" class="img-thumbnail rounded-circle mt-5" alt="profile picture">
-                <p class="username mt-3 mb-1"><?php echo htmlspecialchars($userData['username']); ?> • <span>16 followers</span></p>
+                <p class="username mt-3 mb-1"><?php echo htmlspecialchars($userData['username']); ?> • 
+                    <span>16 followers</span>
+                </p>
                 <p class="biography"><?php echo htmlspecialchars($userData['bio']); ?></p>
                 <p class="education"><?php echo htmlspecialchars($userData['education']); ?></p>
                 <form action="" method="post">
@@ -60,8 +59,7 @@ if (empty($posts)) {
                             <div class="modal-dialog modal-dialog-centered">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="report-userLabel">Are you sure you want to report
-                                            this user?</h5>
+                                        <h5 class="modal-title" id="report-userLabel">Are you sure you want to report this user?</h5>
                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
                                     <form action="" method="post">
@@ -90,9 +88,11 @@ if (empty($posts)) {
                             <?php if (!empty($userPosts[0]['social_linkedin'])) : ?>
                                 <a href="<?php echo htmlspecialchars($userPosts[0]['social_linkedin']); ?>" class="btn btn-outline-primary mb-2"><img src="assets/icons/icon_linkedin.png" alt="linkedin"></a>
                             <?php endif; ?>
+
                             <?php if (!empty($userPosts[0]['social_github'])) : ?>
                                 <a href="<?php echo htmlspecialchars($userPosts[0]['social_github']); ?>" class="btn btn-outline-primary mb-2"><img src="assets/icons/icon_github.png" alt="github"></a>
                             <?php endif; ?>
+
                             <?php if (!empty($userPosts[0]['social_instagram'])) : ?>
                                 <a href="<?php echo htmlspecialchars($userPosts[0]['social_instagram']); ?>" class="btn btn-outline-primary mb-2"><img src="assets/icons/icon_instagram.png" alt="instagram"></a>
                             <?php endif; ?>
@@ -107,51 +107,47 @@ if (empty($posts)) {
         <div>
             <div class="d-flex header mr-auto p-2 ">
                 <div class="">
-                    <h3>My featured projects 💥</h3>
+                    <h3>My featured projects</h3>
                 </div>
-                <div class="p-2"><a href="profile.php?p=<?php echo $userData['id'] ?>" type="submit" name="smashedprojects" class="btn btn-outline-success">All my projects</a></div>
+                <div class="p-2">
+                    <a href="profile.php?p=<?php echo $userData['id'] ?>" name="smashedprojects" class="btn btn-outline-primary">All projects</a>
+                </div>
             </div>
+
             <?php if (!empty($emptyState)) : ?>
                 <div class="empty-state flex-column">
                     <img class="d-block mx-auto" src="assets/images/empty-state.png" alt="emptystate">
                     <h3 class="text-center py-4">Nothing to see here...</h3>
                 </div>
             <?php else : ?>
-
                 <div class="row justify-content-start">
-
                     <?php foreach ($posts as $key => $p) : ?>
-                        <?php
-                        $tags = Post::getTagsFromPost($p[0]);
-                        //var_dump($p[0]);
-                        ?>
+                        <?php $tags = Post::getTagsFromPost($p[0]); ?>
                         <div class="col-12 col-md-6 col-lg-4 p-4">
                             <img src="uploaded_projects/<?php echo $p['image']; ?>" width="100%" height="250px" class="img-project-post" style="object-fit:cover">
                             <div>
                                 <a href="post.php?p=<?php echo $p[0] ?>">
                                     <h2><?php echo $p['title']; ?></h2>
                                 </a>
-
                                 <p class="pe-4"><?php echo $p['description']; ?>
                                     <?php foreach ($tags as $tag) : ?>
                                         <span class="link-primary"><?php echo $tag['tag']; ?></span>
                                     <?php endforeach; ?>
                                 </p>
                             </div>
-
                         </div>
                     <?php endforeach; ?>
-
                 </div>
             <?php endif; ?>
         </div>
+    </div>
 
-        <?php echo include_once('footer.php'); ?>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-        <script src="javascript/follow.js"></script>
-        <script src="javascript/report-user.js"></script>
-        <script src="javascript/smashed.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <?php echo include_once('footer.php'); ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="javascript/follow.js"></script>
+    <script src="javascript/report-user.js"></script>
+    <script src="javascript/smashed.js"></script>
 </body>
-
 </html>

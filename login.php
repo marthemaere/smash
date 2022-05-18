c6951da33a9ec3b05ea95fd15281682489bf1e8c
Joris Hens <joris@goodbytes.be>
2022-05-18 21:26:25 +0200

    fix: fixes #10

--- login.php
+++ login.php
@@ -44,7 +44,7 @@
                     <?php endif;?>
 
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingInput" placeholder="name@student.thomasmore.be" value="r012345@student.thomasmore.be" name="email">
+                        <input type="text" class="form-control" id="floatingInput" placeholder="name@student.thomasmore.be" value="" name="email">
                         <label for="floatingInput">Email</label>
                     </div>
 

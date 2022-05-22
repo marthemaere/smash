b7692018e9993f70afe23a0cab536a1c5a6d7e8e
Jade Apers <jadeapers@telenet.be>
2022-05-22 23:31:46 +0200

    feat: don't show filter following when not logged in

--- index.php
+++ index.php
@@ -1,53 +1,53 @@
 <?php
-    include_once("bootstrap.php");
-    session_start();
-    
-    $conn = Db::getInstance();
-    
-    if (isset($_SESSION['id'])) {
-        $sessionId = $_SESSION['id'];
-        $userDataFromId = User::getUserDataFromId($sessionId);
-    }
+include_once("bootstrap.php");
+session_start();
 
-    $limit = 15;
-    $page = isset($_GET['page']) ? $_GET['page'] : 1;
-    $start = ($page -1) * $limit;
+$conn = Db::getInstance();
 
+if (isset($_SESSION['id'])) {
+    $sessionId = $_SESSION['id'];
+    $userDataFromId = User::getUserDataFromId($sessionId);
+}
+
+$limit = 15;
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$start = ($page - 1) * $limit;
+
+$sorting = 'DESC';
+$posts = Post::getPosts($sorting, $start, $limit);
+
+$sortPopularTags = Tag::sortPopularTagsDesc();
+
+
+$conn = Db::getInstance();
+$result = $conn->query("select count(id) AS id from posts");
+$postCount = $result->fetchAll();
+$total = $postCount[0]['id'];
+$pages = ceil($total / $limit);
+
+if (!empty($_POST['submit-search'])) {
+    $search = $_POST['search'];
+    $posts = Post::search($search);
+    $searched = true;
+}
+
+if (!empty($_POST['ASC'])) {
+    $sorting = 'ASC';
+    $posts = Post::getPosts($sorting, $start, $limit);
+} elseif (!empty($_POST['DESC'])) {
     $sorting = 'DESC';
     $posts = Post::getPosts($sorting, $start, $limit);
+} elseif (!empty($_POST['following']) ) {
+    $posts = Post::filterPostsByFollowing($start, $limit);
+}
 
-    $sortPopularTags = Tag::sortPopularTagsDesc();
-    
-    
-    $conn = Db::getInstance();
-    $result = $conn->query("select count(id) AS id from posts");
-    $postCount= $result->fetchAll();
-    $total= $postCount[0]['id'];
-    $pages= ceil($total / $limit);
-    
-    if (!empty($_POST['submit-search'])) {
-        $search = $_POST['search'];
-        $posts = Post::search($search);
-        $searched = true;
-    }
-    
-    if (!empty($_POST['ASC'])) {
-        $sorting = 'ASC';
-        $posts = Post::getPosts($sorting, $start, $limit);
-    } elseif (!empty($_POST['DESC'])) {
-        $sorting = 'DESC';
-        $posts = Post::getPosts($sorting, $start, $limit);
-    } elseif (!empty($_POST['following'])) {
-        $posts = Post::filterPostsByFollowing($start, $limit);
-    }
-    
-    if (isset($_GET['tag']) && !empty($_GET['tag'])) {
-        $filteredTag = "#" . $_GET['tag'];
-        $posts = Tag::filterPostsByTag($filteredTag);
-        $filtered = true;
-    }
-    
-    /*
+if (isset($_GET['tag']) && !empty($_GET['tag'])) {
+    $filteredTag = "#" . $_GET['tag'];
+    $posts = Tag::filterPostsByTag($filteredTag);
+    $filtered = true;
+}
+
+/*
     if (isset($_GET['tag']) && !empty($_GET['tag'])) {
         $popularTag = $_GET['tag'];
         $posts = Tag::filterPostsByPopularTag($popularTag);
@@ -55,12 +55,14 @@
     }
     */
 
-    if (empty($posts)) {
-        $emptystate = true;
-    }
+if (empty($posts)) {
+    $emptystate = true;
+}
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -76,13 +78,13 @@
         <div class="d-flex flex-wrap justify-content-between align-items-center m-3">
             <div class="btn-group">
                 <button type="button" class="btn btn-primary sort-title">
-                    <?php if (!empty($_POST['ASC'])): ?>
+                    <?php if (!empty($_POST['ASC'])) : ?>
                         <?php echo "Oldest"; ?>
-                    <?php elseif (!empty($_POST['DESC'])): ?>
+                    <?php elseif (!empty($_POST['DESC'])) : ?>
                         <?php echo "Latest"; ?>
-                    <?php elseif (!empty($_POST['following'])): ?>
+                    <?php elseif (!empty($_POST['following'])) : ?>
                         <?php echo "Following"; ?>
-                    <?php else: ?>
+                    <?php else : ?>
                         <?php echo "Latest"; ?>
                     <?php endif; ?>
                 </button>
@@ -101,20 +103,23 @@
                         <li>
                             <a class="dropdown-item sort-oldest" href="#"><input type="submit" name="ASC" value="Oldest"></a>
                         </li>
+                       
+                        <?php if (isset($_SESSION['id'])) : ?>
                         <li>
                             <h6 class="dropdown-header">Filter</h6>
                         </li>
                         <li>
                             <a class="dropdown-item filter-following" href="#"><input type="submit" name="following" value="Following"></a>
                         </li>
+                        <?php endif; ?>
                     </form>
                 </ul>
             </div>
 
-            <?php if (!empty($sortPopularTags)): ?>
+            <?php if (!empty($sortPopularTags)) : ?>
                 <div class="filter-tags">
                     <a href="index.php" class="px-2 btn btn-light">All</a>
-                    <?php foreach ($sortPopularTags as $pTag): ?>
+                    <?php foreach ($sortPopularTags as $pTag) : ?>
                         <a href="index.php?tag=<?php echo str_replace("#", "", $pTag['tag']); ?>" class="px-2 btn btn-light"><?php echo $pTag['tag']; ?></a>
                     <?php endforeach; ?>
                 </div>
@@ -125,45 +130,45 @@
             </div>
         </div>
 
-        <?php if (!empty($searched)): ?>
+        <?php if (!empty($searched)) : ?>
             <div class="d-flex mt-5 ms-3 me-3 alert alert-dark bg-light">
                 <p class="m-0">Search results for: <span class="fw-bold"><?php echo $search; ?></span></p>
             </div>
         <?php endif; ?>
 
-        <?php if (!empty($filtered)): ?>
+        <?php if (!empty($filtered)) : ?>
             <div class="d-flex mt-5 ms-3 me-3 alert alert-dark bg-light">
                 <p class="m-0">Filter by tag: <span class="fw-bold"><?php echo $filteredTag; ?></span></p>
             </div>
         <?php endif; ?>
 
-        <?php if (!empty($filteredPopularTag)): ?>
+        <?php if (!empty($filteredPopularTag)) : ?>
             <div class="d-flex mt-5 ms-3 me-3 alert alert-dark bg-light">
                 <p class="m-0">Filter by tag: <span class="fw-bold"><?php echo $popularTag; ?></span></p>
             </div>
         <?php endif; ?>
 
-        <?php if (isset($emptystate)): ?>
+        <?php if (isset($emptystate)) : ?>
             <div class="empty-state">
                 <img class="d-block mx-auto" src="assets/images/empty-state-weareimd.png" alt="empty state">
             </div>
         <?php endif; ?>
 
         <div class="row justify-content-start">
-            <?php foreach ($posts as $key => $p): ?>
-            
-                <?php if (!isset($_SESSION['id'])) :?>
+            <?php foreach ($posts as $key => $p) : ?>
+
+                <?php if (!isset($_SESSION['id'])) : ?>
                     <?php
-                        $like = new Like();
-                        $like->setPostId($p[0]);
-                        $count = $like->getLikes();
-                        $tags = Post::getTagsFromPost($p[0]);
+                    $like = new Like();
+                    $like->setPostId($p[0]);
+                    $count = $like->getLikes();
+                    $tags = Post::getTagsFromPost($p[0]);
                     ?>
-                    
+
                     <div class="col-12 col-md-6 col-lg-4 p-4">
 
                         <a href="register.php">
-                            <img src="<?php echo $p['image_thumb'];?>" width="100%" height="250px" class="img-project-post" style="object-fit:cover">
+                            <img src="<?php echo $p['image_thumb']; ?>" width="100%" height="250px" class="img-project-post" style="object-fit:cover">
                         </a>
 
                         <div class="">
@@ -171,13 +176,13 @@
                                 <div class="d-flex align-items-center justify-content-start">
                                     <img src="<?php echo $p['profile_pic']; ?>" class="img-profile-post">
                                     <a href="register.php">
-                                        <h4 class="pt-2 ps-2"><?php echo $p['username'];?></h4>
+                                        <h4 class="pt-2 ps-2"><?php echo $p['username']; ?></h4>
                                     </a>
                                 </div>
 
                                 <div class="d-flex align-items-center">
                                     <img src="assets/images/empty-heart.svg" class="like">
-                                    <?php if ($count['COUNT(id)'] === "0"): ?>
+                                    <?php if ($count['COUNT(id)'] === "0") : ?>
                                         <p class="num-of-likes" data-postid="<?php echo $p[0] ?>"><?php ?></p>
                                     <?php else : ?>
                                         <p class="num-of-likes" data-postid="<?php echo $p[0] ?>"><?php echo $count['COUNT(id)'] ?></p>
@@ -188,7 +193,7 @@
                                 <h2><?php echo $p['title']; ?></h2>
                             </a>
                             <p class="pe-4 mb-1 max-num-of-lines"><?php echo $p['description']; ?></p>
-                            <?php foreach ($tags as $tag): ?>
+                            <?php foreach ($tags as $tag) : ?>
                                 <a href="register.php">
                                     <span class="link-primary"><?php echo $tag['tag']; ?></span>
                                 </a>
@@ -196,52 +201,52 @@
                         </div>
                     </div>
 
-                <?php else: ?>
+                <?php else : ?>
                     <?php
-                        $like = new Like();
-                        $like->setPostId($p[0]);
-                        $like->setUserId($_SESSION['id']);
-                        $isLiked = $like->isPostLikedByUser();
-                        $count = $like->getLikes();
-                        $tags = Post::getTagsFromPost($p[0]);
+                    $like = new Like();
+                    $like->setPostId($p[0]);
+                    $like->setUserId($_SESSION['id']);
+                    $isLiked = $like->isPostLikedByUser();
+                    $count = $like->getLikes();
+                    $tags = Post::getTagsFromPost($p[0]);
                     ?>
                     <div class="col-12 col-md-6 col-lg-4 p-4">
 
-                        <a href="post.php?p=<?php echo $p[0]?>">
-                            <img src="<?php echo $p['image_thumb'];?>" width="100%" height="250px" class="img-project-post" style="object-fit:cover">
+                        <a href="post.php?p=<?php echo $p[0] ?>">
+                            <img src="<?php echo $p['image_thumb']; ?>" width="100%" height="250px" class="img-project-post" style="object-fit:cover">
                         </a>
 
                         <div>
                             <div class="d-flex justify-content-between align-items-center py-2">
                                 <div class="d-flex align-items-center justify-content-start">
                                     <img src="<?php echo $p['profile_pic']; ?>" class="img-profile-post">
-                                    <a href="profile.php?p=<?php echo $p['user_id'];?>">
-                                        <h4 class="pt-2 ps-2"><?php echo $p['username'];?></h4>
+                                    <a href="profile.php?p=<?php echo $p['user_id']; ?>">
+                                        <h4 class="pt-2 ps-2"><?php echo $p['username']; ?></h4>
                                     </a>
                                 </div>
                                 <form class="" action="" method="post">
                                     <div class="d-flex align-items-center">
-                                        <?php if (!$isLiked): ?>
+                                        <?php if (!$isLiked) : ?>
                                             <img src="assets/images/empty-heart.svg" name="like" class="like notLiked" id="likePost" data-userid="<?php echo $_SESSION['id'] ?>" data-postid="<?php echo $p[0] ?>">
-                                        <?php if ($count['COUNT(id)'] === "0"): ?>
-                                            <p class="num-of-likes" data-postid="<?php echo $p[0] ?>"><?php ?></p>
+                                            <?php if ($count['COUNT(id)'] === "0") : ?>
+                                                <p class="num-of-likes" data-postid="<?php echo $p[0] ?>"><?php ?></p>
+                                            <?php else : ?>
+                                                <p class="num-of-likes" data-postid="<?php echo $p[0] ?>">
+                                                    <?php echo $count['COUNT(id)'] ?></p>
+                                            <?php endif; ?>
                                         <?php else : ?>
-                                        <p class="num-of-likes" data-postid="<?php echo $p[0] ?>">
-                                            <?php echo $count['COUNT(id)'] ?></p>
-                                        <?php endif; ?>
-                                        <?php else: ?>
                                             <img src="assets/images/liked-heart.svg" name="like" class="like notLiked" id="likePost" data-userid="<?php echo $_SESSION['id'] ?>" data-postid="<?php echo $p[0] ?>">
                                             <p class="num-of-likes" data-postid="<?php echo $p[0] ?>">
-                                            <?php echo $count['COUNT(id)'] ?></p>
+                                                <?php echo $count['COUNT(id)'] ?></p>
                                         <?php endif; ?>
                                     </div>
                                 </form>
                             </div>
-                            <a href="post.php?p=<?php echo $p[0]?>">
+                            <a href="post.php?p=<?php echo $p[0] ?>">
                                 <h2><?php echo $p['title']; ?></h2>
                             </a>
                             <p class="pe-4 mb-1 max-num-of-lines"><?php echo $p['description']; ?></p>
-                            <?php foreach ($tags as $tag): ?>
+                            <?php foreach ($tags as $tag) : ?>
                                 <a href="index.php?tag=<?php echo str_replace("#", "", $tag['tag']); ?>" class="link-primary bg-transparent border-0 p-0"><?php echo $tag['tag']; ?></a>
                             <?php endforeach; ?>
                         </div>
@@ -251,10 +256,10 @@
 
             <nav aria-label="Results pages">
                 <ul class="pagination justify-content-center">
-                    <?php for ($i=1; $i<= $pages; $i++): ?>
-                    <li class="page-item" aria-current="page">
-                        <a href="index.php?page=<?= $i; ?>" class="page-link"><?= $i; ?></a>
-                    </li>
+                    <?php for ($i = 1; $i <= $pages; $i++) : ?>
+                        <li class="page-item" aria-current="page">
+                            <a href="index.php?page=<?= $i; ?>" class="page-link"><?= $i; ?></a>
+                        </li>
                     <?php endfor; ?>
                 </ul>
             </nav>
@@ -267,4 +272,5 @@
     <script src="javascript/like.js"></script>
     <script src="javascript/smashed.js"></script>
 </body>
+
 </html>

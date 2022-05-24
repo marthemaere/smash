a9f703b4eaad973895dce1613531189bbd5e8d79
Fien Gérardi <78747974+fgrardi@users.noreply.github.com>
2022-05-24 23:44:46 +0200

    fix: changed allowed extensions of files so a user can upload a gif

--- classes/Upload.php
+++ classes/Upload.php
@@ -2,39 +2,38 @@
     use Cloudinary\Configuration\Configuration;
     use Cloudinary\Api\Upload\UploadApi;
 
-
-
-    /* 
+    /*
         Upload class to easily upload files to Cloudinary
         Created by @iamgoodbytes
-    */  
-    class Upload {
-        private static $allowedExtensions = ['jpg', 'jpeg', 'png', 'pdf', 'svg'];
+    */
+
+    class Upload
+    {
+        private static $allowedExtensions = ['jpg', 'jpeg', 'png', 'pdf', 'svg', 'gif'];
         private static $fileName;
         private static $fileTempName;
         private static $fileSize;
         private static $fileError;
         private static $fileExtension;
 
-        private static function isExtensionAllowed($extention){
-            if(in_array($extention, self::$allowedExtensions)) {
+        private static function isExtensionAllowed($extention)
+        {
+            if (in_array($extention, self::$allowedExtensions)) {
                 return true;
-            }
-            else {
+            } else {
                 return false;
             }
         }
 
-        public static function upload($file) {
+        public static function upload($file)
+        {
             $config = Configuration::instance();
             $config->cloud->cloudName = 'weareimd';
             $config->cloud->apiKey = '524548632519475';
             $config->cloud->apiSecret = 'ZVsNoMjJ9OYJn7FZInQ9hvJwhIM';
             $config->url->secure = true;
 
-
-
-            // prepare file details
+            // Prepare file details
             self::$fileName = $file['name'];
             self::$fileTempName = $file['tmp_name'];
             self::$fileSize = $file['size'];
@@ -42,23 +41,23 @@
             $fileExt = explode('.', self::$fileName);
             self::$fileExtension = strtolower(end($fileExt)); //check in lowercase
 
-            // check for file errors
-            if(self::$fileError){
+            // Check for file errors
+            if (self::$fileError) {
                 throw new Exception("There was an error uploading your file.");
             }
 
-            // check the extension
-            if(!self::isExtensionAllowed(self::$fileExtension)) {
-                throw new Exception("This file type is not allowed. Only the following file extension are allowed: " . self::getAllowedExtension() );
+            // Check the extension
+            if (!self::isExtensionAllowed(self::$fileExtension)) {
+                throw new Exception("This file type is not allowed. Only the following file extension are allowed: " . self::getAllowedExtension());
             }
 
-            // check the filesize
-            if(!self::isFileSizeAllowed()){
+            // Check the filesize
+            if (!self::isFileSizeAllowed()) {
                 throw new Exception("This file is too large. The maximum file size is 2MB.");
             }
 
-            // all is well, let's upload to Cloudinary
-            $result = (new UploadApi())->upload(self::$fileTempName, [ 
+            // All is well, let's upload to Cloudinary
+            $result = (new UploadApi())->upload(self::$fileTempName, [
                 "eager" => [
                   ["width" => 500, "height" => 375, "crop" => "lfill", "gravity" => 'north', "resize" => "limit"],
                 ]
@@ -69,21 +68,20 @@
                 "image_thumb" => $result['eager'][0]['secure_url']
             ];
             return $uploadResult;
-
         }
 
-        private static function isFileSizeAllowed() {
-            // check if size is less than 2MB
-            if(self::$fileSize < 2097152) {
+        private static function isFileSizeAllowed()
+        {
+            // Check if size is less than 2MB
+            if (self::$fileSize < 2097152) {
                 return true;
-            }
-            else {
+            } else {
                 return false;
             }
         }
 
-        private static function getAllowedExtension() {
+        private static function getAllowedExtension()
+        {
             return implode(", ", self::$allowedExtensions);
         }
-
     }

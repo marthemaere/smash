6d61632339644829233633416f8b303b04cce24e
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-26 18:32:04 +0200

    feat: an admin can delete anyones post

--- post.php
+++ post.php
@@ -24,6 +24,13 @@
         } else {
             $isModerator = false;
         }
+
+        // Check if user is admin
+        if ($userDataFromId['is_admin'] == 1) {
+            $isAdmin = true;
+        } else {
+            $isAdmin = false;
+        }
              
         // Check if post is reported by user who is logged in
         $report = new Report();
@@ -179,7 +186,7 @@
                                 <a class="btn btn-danger disabled" data-bs-toggle="modal" href="#reportPost" id="report-btn" role="button">Reported</a>
                                 <?php endif; ?>
                             <?php endif; ?>
-                            <?php if ($_SESSION['id'] == $projectData['user_id'] || $isModerator === true): ?>
+                            <?php if ($_SESSION['id'] == $projectData['user_id'] || $isModerator === true || $isAdmin === true): ?>
                             <a class="btn btn-outline-danger ms-2" data-bs-toggle="modal" href="#deleteProject" role="button">Delete</a>
                             <?php endif; ?>
                         </form>
